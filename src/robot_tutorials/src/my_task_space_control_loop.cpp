7d4c69a0be1d7595cbe4eaf17a888b7090576028
VincenzoScamarcio <82524590+VincenzoScamarcio@users.noreply.github.com>
2021-04-16 16:58:05 +0200

    Add files via upload

--- src/robot_tutorials/src/my_task_space_control_loop.cpp
+++ src/robot_tutorials/src/my_task_space_control_loop.cpp
@@ -13,21 +13,21 @@ int main(int, char**) {
   double tol = 1e-3;
   
   //doesn't work using this definition 
-  //CartesianState mf("moving_frame"), tf("target_frame");
-  //mf.set_pose(Eigen::VectorXd::Random(7));
-  //tf.set_pose(Eigen::VectorXd::Random(7));
+  CartesianState mf("moving_frame"), tf("target_frame");
+  mf.set_pose(Eigen::VectorXd::Random(7));
+  tf.set_pose(Eigen::VectorXd::Random(7));
   
   //it works using this definition
   //CartesianPose mf = CartesianPose::Random("moving_frame"); 
   //CartesianPose tf = CartesianPose::Random("target_frame");
   
   //it works if I manually define pose of the frames
-  CartesianState mf("moving_frame");
-  mf.set_position(Eigen::Vector3d(0,1,0));  
-  mf.set_orientation(Eigen::Quaterniond(1,0,0,0));
-  CartesianState tf("target_frame");
-  tf.set_position(Eigen::Vector3d(1,0,0));  
-  tf.set_orientation(Eigen::Quaterniond(0,1,0,0));
+  //CartesianState mf("moving_frame");
+  //mf.set_position(Eigen::Vector3d(0,1,0));  
+  //mf.set_orientation(Eigen::Quaterniond(1,0,0,0));
+  //CartesianState tf("target_frame");
+  //tf.set_position(Eigen::Vector3d(1,0,0));  
+  //tf.set_orientation(Eigen::Quaterniond(0,1,0,0));
   
   std::cout << mf << std::endl;
   std::cout << tf << std::endl;
@@ -42,7 +42,8 @@ int main(int, char**) {
   do {
      CartesianTwist desired_twist = ls.evaluate(mf);
      std::cout << "The desired linear & angular velocity to reach target_frame is: \n"         <<desired_twist << "\n" << std::endl;
-     CartesianState new_mf = mf + dt * desired_twist;
+     CartesianState new_mf = dt * desired_twist +mf;
+     //CartesianPose new_mf = dt * desired_twist + mf;
      std::cout << new_mf << std::endl;
      distance = dist(new_mf,tf,CartesianStateVariable::POSE);
      std::cout << "distance in position + orientation to attractor: " << std::to_string(distance) << std::endl;
